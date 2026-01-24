90df2de2ee00811445cac3a577c340b1f034c889
Lovnish Verma <princelv84@gmail.com>
2026-01-24 21:48:09 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const CONFIG = {
-    SHEET_URL: 'https://script.google.com/macros/s/AKfycbzGbZ39b67jJ2NPODCW4K3zqlf6p6-bq8vPRHmCdNjOxl37MT9KdndnCR8vw55qv7dN/exec',
-    CLOUDINARY_NAME: 'dy8up08qd',
-    CLOUDINARY_PRESET: 'studenthub_preset'
-};
